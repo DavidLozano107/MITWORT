81e083d49c2ae46608924e0c581f90acabe367bd
Cristian Alejandro Pérez Alvis <55390186+Crisops@users.noreply.github.com>
2020-10-02 19:04:10 -0500

    Complete Login con sus respectivas validaciones

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import logo from "./img/wave1.png";
 import backImage from "./img/bg.svg";
-
 import LoginForm from "../loginForm/";
 import SingUpForm from "../signUpForm/";
+import ForgotPassword from "../forgotPassword/"
+
 
 class Login extends Component {
+  
   state = {
     register: false,
+  };
+  state = {
+    forgotPassword: false,
   };
 
   onRegisterActive = (e) => {
@@ -16,7 +21,16 @@ class Login extends Component {
     });
   };
 
+  onForgotPasswortActive = (e) => {
+    e.preventDefault();
+    this.setState({
+      forgotPassword: true,
+    });
+  };
+
+
   render() {
+    const { register, forgotPassword } = this.state;
     return (
       <>
         <img className="wave" src={logo} alt="logo"></img>
@@ -25,13 +39,17 @@ class Login extends Component {
             <img src={backImage} alt=""></img>
           </div>
           <div className="login-container">
-            {/*----------------------------------------------------- */}
-            {this.state.register ? (
-              <SingUpForm />
-            ) : (
-              <LoginForm onRegisterActive={this.onRegisterActive} />
+            {/* ----------------------------------------------------- */}
+
+            {register && <SingUpForm />}
+            {forgotPassword && <ForgotPassword />}
+
+            {register || forgotPassword ? null : (
+              <LoginForm
+                onRegisterActive={this.onRegisterActive}
+                onForgotPasswortActive={this.onForgotPasswortActive}
+              />
             )}
-            {/* ---------------------------------------------------------------------------*/}
           </div>
         </div>
       </>
--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import {withFormik, Field} from "formik";
 
 import logoHome from "./img/avatarMit.svg";
+
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 
-export default class Login extends Component {
+
+class Login extends Component {
   render() {
+    const {
+      handleSubmit,
+      isSubmitting,
+      errors,
+      isValid,
+      touched,
+    } = this.props;
     return (
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <img className="avatar" src={logoHome} alt="MITWORT"></img>
           <h2>WELCOME</h2>
           <div className="input-div one">
@@ -19,7 +29,15 @@ export default class Login extends Component {
               </i>
             </div>
             <div>
-              <input className="input" type="text" placeholder="Email"></input>
+              <Field
+               className="input"
+               name="email"
+               type="email" 
+               placeholder="Email" > 
+              </Field>
+              {errors.email && touched.email && 
+                <div className="errorPass">{errors.email}</div>
+              }
             </div>
           </div>
           <div className="input-div two">
@@ -29,24 +47,65 @@ export default class Login extends Component {
               </i>
             </div>
             <div>
-              <input
+              <Field
                 className="input"
+                name="password"
                 type="password"
-                placeholder="Password"
-              ></input>
+                placeholder="Password">
+              </Field>
+              {errors.password && touched.password && 
+                <div className="errorPass">{errors.password}</div>
+              }
             </div>
           </div>
-          <button>Forwort Password</button>
-
+          <button onClick={this.props.onForgotPasswortActive}>Forgort Password</button>
           <button onClick={this.props.onRegisterActive}>Sign Up</button>
-          <input
-            className="btn"
+          <button
             type="submit"
+            className={`btn ${isSubmitting || !isValid ? 'disabled' : ''}`}
+            disabled={isSubmitting || !isValid}
             value="Login"
             // onClick={onIngreso}
-          ></input>
+          >Submit</button>
         </form>
       </div>
     );
   }
 }
+              /*Maneja el estado del Form*/
+export default withFormik({
+
+  /*Inicializa los valores al momento de cargar nuestro Form*/
+    mapPropsToValues(/*Se pueden Utilizar los props de la linea 19*/){
+      return {
+        email: '',
+        password: '',
+      }
+    },
+    /*------------------------------------------------------------*/
+
+    /*-------- Valida los campos y muestra un error si es necesario----*/
+    validate(values){
+      const errors = {};
+
+      if( !/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(values.email)) {
+        errors.email = "Invalid email address";
+      }else if (!values.password) {
+        errors.password = "Password is required"
+      }else if(values.password.length < 8){
+        errors.password = "Password must be at least 8 characters"
+      }
+
+      return errors;
+
+    },
+    /*---------------------------------------------------------------*/
+
+  /*-----Enviar controlador. Esto debe pasarse onSubmit={props.handleSubmit}. Para obtener más información sobre el proceso de envío*/
+  handleSubmit(values, formikBag){
+    formikBag.setSubmitting(false);
+    console.log(values)
+    
+  }
+  /*---------------------------------------------*/
+})(Login);
